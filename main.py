4954fedf488c2b4a7f5b9109bc3310d163d7f64b
Sarvesh Bhavedia <sarveshbhavedia71@gmail.com>
2026-03-18 21:17:10 +0530

    Implement Tetris class and initialize pygame
    Added Tetris class and initialized game resources.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("HELLO")
+# bonjour a tous je suis J et mon ami koustubh
+
+print("Hello World")
+
+
+import sys 
+import pygame 
+
+class Tetris:
+  # this is an overall class to manage game assests and behaviour 
+
+  def __init__(self):
+    #initalize game and create resources
+    pygame.init()
+    self.screen = pygame.display.set_mode((1920,1080)
+    pygame.display.set_caption("TETRIS")
+
+
+    # for now i have added a main class thats tertris and a def that has screen resolution and when th game opens its shows tetris on its above an drecent apps

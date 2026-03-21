cc925cbe44b0c9e01484063c75badeadc5c61433
Koustubh7802 <shrivastavakoustubh164@gmail.com>
2026-03-21 21:54:02 +0530

    Homepage buttons added to navigate to different pages.

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+# This is a separate file of the constant values which are used in the project
+# to make the things more clear and easy.
+
+# colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+YELLOW = (255,255,50)
+GREEN = (0,255,0)
--- main.py
+++ main.py
@@ -2,13 +2,10 @@
 
 import sys 
 import pygame
+import constants
 
 # what i have done currently: (i will remove this comment on my next updation)
-# 1. made a window object of the class Tetris
-# 2. applied a while loop to display the screen and 
-#    just checked using draw rect function that is the loop working or not.
-
-# Waiting for the list of buttons from sarvesh which i will add in the homescreen
+# 1. added three buttons (not working currently) but with a hover feature.
 
 class Tetris:
     # this is an overall class to manage game assests and behaviour 
@@ -18,33 +15,78 @@ class Tetris:
         pygame.init()
         self.screen = pygame.display.set_mode((1080,720))
         pygame.display.set_caption("TETRIS")
+        self.font = pygame.font.Font('freesansbold.ttf',20) # fonts added
 
 
         # for now i have added a main class thats tertris and a def that has screen resolution and when th game opens its shows tetris on its above an drecent apps
 
+class Buttons:
+    def __init__(self,name,pos_x,pos_y,width,height):
+        self.name = name
+        self.x = pos_x
+        self.y = pos_y
+        self.w = width
+        self.h = height
+        self.hover = 0
+        self.state = 0
 
+        self.button_rect = pygame.rect.Rect((self.x,self.y),(self.w,self.h))
+
+    def draw_buttons(self):
+        pygame.draw.rect(window.screen, constants.YELLOW, (self.x - 10,self.y - 10,self.w + 20,self.h + 20)) # border
+        pygame.draw.rect(window.screen, constants.WHITE, (self.x,self.y,self.w,self.h))
+
+        if self.hover == 1:
+            pygame.draw.rect(window.screen, constants.GREEN, (self.x,self.y,self.w,self.h))
+
+        button_text = window.font.render(self.name,True,constants.BLACK)
+        window.screen.blit(button_text,(self.x, self.y))
+
+    
 
 window = Tetris()
 running = True
 
-list_of_buttons = [0,1,2,3,4,5] #sarvesh has to name them
+
+new_game = Buttons('NEW GAME',100,100,200,60)
+records = Buttons('RECORDS',100,200,200,60)
+settings = Buttons('SETTINGS',100,300,200,60)
 
 while running:
   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        mouse_pos = pygame.mouse.get_pos()
     
         if event.type == pygame.MOUSEBUTTONDOWN:
             # (temporary) draws white squares on the position of mouse click of size 50,50
-            x,y = pygame.mouse.get_pos()
+            x,y = mouse_pos[0],mouse_pos[1]
             pygame.draw.rect(window.screen,(255,255,255),(x,y,50,50))
             pass
+        
+        if mouse_pos and new_game.button_rect.collidepoint(mouse_pos):
+            new_game.hover = 1
+        else:
+            new_game.hover = 0
+        
+        if mouse_pos and records.button_rect.collidepoint(mouse_pos):
+            records.hover = 1
+        else:
+            records.hover = 0
+        
+        if mouse_pos and settings.button_rect.collidepoint(mouse_pos):
+            settings.hover = 1
+        else:
+            settings.hover = 0
+        
 
-        if list_of_buttons == 1:
-            # move to gamepage
-            pass
+        
     
+    new_game.draw_buttons()
+    records.draw_buttons()
+    settings.draw_buttons()
     pygame.display.update()
 
 pygame.quit()
